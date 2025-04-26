8c3649c6c49ee793754d089cc1fed272b96478de
leleah <alya.leah.13@gmail.com>
2025-04-26 17:19:33 +0300

    modules

--- vectors/more replication/vector5.v
+++ vectors/more replication/vector5.v
@@ -5,9 +5,9 @@ module top_module (
     // The output is XNOR of two vectors created by 
     // concatenating and replicating the five inputs.
     assign out[24:20] = ~{5{a}} ^ {a,b,c,d,e};
-    assign out[19:15] = ~{4{b}} ^ {a,b,c,d,e};
-    assign out[14:10] = ~{4{c}} ^ {a,b,c,d,e};
-    assign out[9:5] = ~{4{d}} ^ {a,b,c,d,e};
-    assign out[4:0] = ~{4{e}} ^ {a,b,c,d,e};
+    assign out[19:15] = ~{5{b}} ^ {a,b,c,d,e};
+    assign out[14:10] = ~{5{c}} ^ {a,b,c,d,e};
+    assign out[9:5] = ~{5{d}} ^ {a,b,c,d,e};
+    assign out[4:0] = ~{5{e}} ^ {a,b,c,d,e};
 
 endmodule
